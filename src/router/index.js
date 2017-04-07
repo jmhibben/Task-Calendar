3b67f0f2a65404da29830939bb2e1df24c3e5ea9
James Hibben <jmhibben@gmail.com>
2017-04-07 14:39:06 -0500

    Added Calendar to the routerz

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Calendar from '@/components/Calendar'
 
 Vue.use(Router)
 
@@ -8,8 +8,9 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      alias: '/calendar',
+      name: 'Calendar',
+      component: Calendar
     }
   ]
 })
