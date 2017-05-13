d303aa9d0b19b1e3494731348db0c5f53afca72e
James Hibben <jmhibben@gmail.com>
2017-05-13 00:13:35 -0500

    Added logging to Store

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,55 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import chalk from 'chalk'
+import Loggerr from 'loggerr'
 
 // This lets us pass the store object through the entire app as `this.$state`
 Vue.use(Vuex)
+
+// Server colors
+let error = chalk.red.bold
+let warn = chalk.red
+let info = chalk.cyan
+let debug = chalk.green.bold
+
+const l = {
+  emergency: Loggerr.EMERGENCY,
+  alert: Loggerr.ALERT,
+  critical: Loggerr.CRITICAL,
+  error: Loggerr.ERROR,
+  warning: Loggerr.WARNING,
+  notice: Loggerr.NOTICE,
+  info: Loggerr.INFO,
+  debug: Loggerr.DEBUG
+}
+
+let logger = new Loggerr({
+  streams: Loggerr.levels.map(() => {
+    return console.log
+  }),
+  formatter: Loggerr.levels.map((date, level, data) => {
+    let color
+    switch (Loggerr.levels.indexOf(level)) {
+      case l.emergency:
+      case l.alert:
+      case l.critical:
+      case l.error:
+        color = error
+        break
+      case l.warning:
+      case l.notice:
+        color = warn
+        break
+      case l.info:
+        color = info
+        break
+      case l.debug:
+      default:
+        color = debug
+    }
+    return color(date + ': ' + data.msg)
+  })
+})
 
 const apihost = 'http://localhost:8079'
 
@@ -25,7 +72,7 @@ export default new Vuex.Store({
     // Uses the auth code provided by OAuth redirected back to the app
     // We're storing this to ensure that we can refresh the token if necessary
     setAuthUrl: (state, auth) => {
-      // console.log(auth.url)
+      // logger.debug(auth)
       state.auth.url = auth.url
     },
     setTokenRecieved: (state) => {
@@ -39,13 +86,15 @@ export default new Vuex.Store({
     //  https://vuex.vuejs.org/en/actions.html#dispatching-actions
     //  https://vuex.vuejs.org/en/actions.html#dispatching-actions-in-components
     apiShowLogin ({ commit }) {
-      // let xhr = sendRequest('GET', '/api')
+      // let xhr = sendRequest('GET', '/auth/url')
+      // setImmediate(xhr)
       // console.log(xhr)
       let xhr = new XMLHttpRequest()
 
-      xhr.addEventListener('readystatechange', function () {
+      xhr.addEventListener('readystatechange', () => {
         if (xhr.readyState === 4) {
-          console.log(xhr)
+          // console.log(xhr)
+          logger.debug(xhr)
           // console.log(xhr.responseText) // debug
           let response = JSON.parse(xhr.responseText)
           // console.log(response)
@@ -61,8 +110,13 @@ export default new Vuex.Store({
       // console.log(xhr)
     },
     apiSetToken ({ commit }, authCode) {
+      // let xhr = sendRequest('POST', `/code/${authCode}`)
+      // setImmediate(xhr)
+      // console.log(xhr)
+      // let xhr = sendRequest('POST', `/code/${authCode}`)
+      // console.log(xhr)
       let xhr = new XMLHttpRequest()
-      xhr.addEventListener('readystatecange', function () {
+      xhr.addEventListener('readystatecange', () => {
         if (xhr.readyState === 4 && xhr.status === 204) {
           console.log(xhr.response)
           commit('setTokenRecieved')
@@ -78,12 +132,15 @@ export default new Vuex.Store({
 })
 
 // function sendRequest (method, path) {
-//   let api = 'http://localhost:8079'
+//   let api = `${apihost}/api`
 //   let xhr = new XMLHttpRequest()
 //   xhr.open(method, `${api}${path}`)
 //   xhr.withCredentials = true
 //   xhr.addEventListener('readystatechange', () => {
+//     // console.log('Response recieved')
+//     // console.log(xhr)
 //     if (xhr.readyState === 4) return xhr
 //   })
+//   xhr.setRequestHeader('cache-control', 'no-cache')
 //   xhr.send()
 // }
